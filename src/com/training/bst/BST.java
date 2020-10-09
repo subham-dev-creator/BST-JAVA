c0b2e1f7dc064a75c841663a8165127f9dbd8e3c
subhamstark <subham@sap.com>
2020-10-10 01:08:09 +0530

    UC2 Done

--- src/com/training/bst/BST.java
+++ src/com/training/bst/BST.java
@@ -14,13 +14,33 @@ public class BST<T extends Comparable<T>> {
         else
         {
             if(root.getData().compareTo(data)>0){
-                return insertHelper(data,root.getRight());
+                root.setRight(insertHelper(data,root.getRight()));
             }
             else
             {
-                return insertHelper(data,root.getLeft());
+                root.setLeft(insertHelper(data,root.getLeft()));
             }
+            return root;
         }
+    }
+
+    public int size(){
+        return sizeHelper(root);
+    }
+
+    private int sizeHelper(Node<T> root) {
+        if(root==null)
+            return 0;
+        else
+        {
+            int l =sizeHelper(root.getLeft());
+            int r =sizeHelper(root.getRight());
+            return max(l,r)+1;
+        }
+    }
+
+    private int max(int a, int b) {
+        return a>b ? a : b;
     }
 
     public static void main(String[] args) {
@@ -28,6 +48,18 @@ public class BST<T extends Comparable<T>> {
         bst.insert(56);
         bst.insert(30);
         bst.insert(70);
+        bst.insert(22);
+        bst.insert(40);
+        bst.insert(60);
+        bst.insert(95);
+        bst.insert(11);
+        bst.insert(3);
+        bst.insert(16);
+        bst.insert(65);
+        bst.insert(63);
+        bst.insert(67);
+
+        System.out.println(bst.size());
     }
 
 }
